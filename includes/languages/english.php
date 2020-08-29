1f79664601e02ebaa0cb7c79da826f2196a5bb48
arcaniafr <56791020+arcaniafr@users.noreply.github.com>
2020-08-29 03:11:56 +0200

    Create english.php

---
+++ includes/languages/english.php
@@ -0,0 +1,15 @@
+<?php // French language for the site
+	$lang["header-index"] = "Home";
+	$lang["header-forum"] = "Forum";
+	$lang["header-bugtracker"] = "Bugtracker";
+	$lang["header-account"] = "My account";
+	$lang["header-help"] = "Help";
+	$lang["header-total-cart"] = "Total";
+	$lang["header-view-cart"] = "Show my basket";
+	$lang["header-username"] = "Username";
+	$lang["header-password"] = "Password";
+	$lang["header-remember-me"] = "Remember me";
+	$lang["header-connect-me"] = "Connect";
+	$lang["header-lost-pass"] = "Lost password ?";
+	$lang["header-register"] = "Register";
+?>
