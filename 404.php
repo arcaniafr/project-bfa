4ad9adcfe48dafd424ad5ab4b2b14770e79712fb
arcaniafr <56791020+arcaniafr@users.noreply.github.com>
2020-08-29 03:38:34 +0200

    Create 404.php

---
+++ 404.php
@@ -0,0 +1,78 @@
+<?php
+	session_start();
+	include("core/config.php");
+	include('includes/languages/french.php'); // temporaire, voir le changement de langue par la suite.
+?>
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<title><?=$_config['website-title']?></title>
+<meta name="description" content="Gaming HTML Template based on Bootstrap">
+<meta name="keywords" content="gaming, game, community, template, html, bootstrap, webpack">
+<meta name="author" content="nK">
+<link rel="icon" type="image/png" href="<?=$_config["website-icon"]?>">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:300,400,700">
+<link rel="stylesheet" href="assets/css/bootstrap.min.css" />
+<link rel="stylesheet" href="assets/css/custom.css">
+</head>
+<body>
+
+<div class="preloader-wrapp">
+<h2><?=$_config['website-title']?></h2>
+<div class="preloader"></div>
+</div>
+
+<?php require_once("includes/header.php");?>
+
+<div class="content-wrap arc-404">
+
+<section class="arc-banner banner-top arc-banner-parallax full">
+<div class="image" data-speed="0.4">
+<img src="assets/images/banner-bg.jpg" alt="" class="jarallax-img">
+</div>
+<div class="info">
+<div>
+<div class="container">
+<div class="text-center">
+<h2 class="h1">404</h2>
+<h3>Page Not Found ;(</h3>
+<form action="search.html">
+<div class="arc-input">
+<input type="text" name="search" placeholder="Site Search">
+</div>
+</form>
+</div>
+</div>
+</div>
+</div>
+</section>
+
+
+<?php require_once("includes/footer.php");?>
+
+</div>
+
+<div class="modal" tabindex="-1" role="dialog" id="loading-modal">
+  <div class="modal-dialog modal-sm" role="document">
+    <div class="modal-content">
+      <div class="modal-body">
+	  <center>
+			<h4>Loading...</h4>
+			<div class="mfp-preloader preloader"></div>
+			</center>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+<script src="assets/js/scripts.js"></script>
+
+</body>
+</html>
